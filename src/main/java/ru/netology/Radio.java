a658855d25843ad3631086675c3bba2f4ab69814
Yurii Trysov <87304775+Kell0702@users.noreply.github.com>
2021-09-22 19:02:47 +0300

    Final commit

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,7 +1,7 @@
 package ru.netology;
 
 public class Radio {
-    String name;
+    String name = "Radio";
     private int maxRadioStation = 9;
     private int minRadioStation = 0;
     private int currentRadioStation = minRadioStation;
@@ -19,78 +19,85 @@ public class Radio {
     }
 
     public void setCurrentRadioStation(int currentRadioStation) {
-//        if (getRadioStation() < getMinRadioStation()) {
-//            return;
-//        }
-//        if (getRadioStation() > getMaxRadioStation()) {
-//            return;
-//        }
+        if (currentRadioStation < minRadioStation) {
+            return;
+        }
+        if (currentRadioStation > maxRadioStation) {
+            return;
+        }
         this.currentRadioStation = currentRadioStation;
     }
 
     public int nextStation() {
         setCurrentRadioStation(getRadioStation() + 1);
-        if (currentRadioStation < getMaxRadioStation()) ;
-        else currentRadioStation = getMinRadioStation();
+        if (currentRadioStation < getMaxRadioStation()) {
+        } else {
+            currentRadioStation = getMinRadioStation();
+        }
         return currentRadioStation;
     }
 
     public int revStation() {
         setCurrentRadioStation(getRadioStation() - 1);
-        if (currentRadioStation > getMinRadioStation()) ;
-        else currentRadioStation = getMaxRadioStation();
+        if (currentRadioStation > getMinRadioStation()) {
+        } else {
+            currentRadioStation = getMaxRadioStation();
+        }
         return currentRadioStation;
     }
 
-    public int setRadioStation0() {
-        setCurrentRadioStation(getMinRadioStation());
-        int radio = currentRadioStation;
-        return radio;
+    public int setRadioStation(int nomberStation) {
+        setCurrentRadioStation(getRadioStation());
+        currentRadioStation = nomberStation;
+        return nomberStation;
     }
-    public int setRadioStation1() {
-        setCurrentRadioStation(getMinRadioStation());
-        int radio = setRadioStation0() + 1;
-        return radio;
-    }
-    public int setRadioStation2() {
-        setCurrentRadioStation(getMinRadioStation());
-        int radio = setRadioStation1() + 1;
-        return radio;
-    }
-    public int setRadioStation3() {
-        setCurrentRadioStation(getMinRadioStation());
-        int radio = setRadioStation2() + 1;
-        return radio;
-    }
-    public int setRadioStation4() {
-        setCurrentRadioStation(getMinRadioStation());
-        int radio = setRadioStation3() + 1;
-        return radio;
-    }
-    public int setRadioStation5() {
-        setCurrentRadioStation(getMinRadioStation());
-        int radio = setRadioStation4() + 1;
-        return radio;
-    }
-    public int setRadioStation6() {
-        setCurrentRadioStation(getMinRadioStation());
-        int radio = setRadioStation5() + 1;
-        return radio;
-    }
-    public int setRadioStation7() {
-        setCurrentRadioStation(getMinRadioStation());
-        int radio = setRadioStation6() + 1;
-        return radio;
-    }
-    public int setRadioStation8() {
-        setCurrentRadioStation(getMinRadioStation());
-        int radio = setRadioStation7() + 1;
-        return radio;
-    }
-    public int setRadioStation9() {
-        setCurrentRadioStation(getMinRadioStation());
-        int radio = setRadioStation8() + 1;
-        return radio;
+
+    public static class Volume {
+        String name = "Volume";
+        private int maxVolume = 10;
+        private int minVolume = 0;
+        private int currentVolume = 3;
+
+        public int getMaxVolume() {
+            return maxVolume;
+        }
+
+        public int getMinVolume() {
+            return minVolume;
+        }
+
+        public int getCurrentVolume() {
+            return currentVolume;
+        }
+
+        public void setCurrentVolume(int currentVolume) {
+            if (currentVolume < getMinVolume()) {
+                return;
+            }
+            if (currentVolume > getMaxVolume()) {
+                return;
+            }
+            this.currentVolume = currentVolume;
+        }
+
+        public int setUpVolume() {
+            setCurrentVolume(getCurrentVolume() + 1);
+            if (currentVolume < getMaxVolume()) {
+            } else {
+                currentVolume = getMaxVolume();
+            }
+            return currentVolume;
+        }
+
+        public int setDownVolume() {
+            setCurrentVolume(getCurrentVolume() - 1);
+            if (currentVolume > getMinVolume()) {
+            } else {
+                currentVolume = getMinVolume();
+            }
+            return currentVolume;
+        }
+
     }
 
 }
